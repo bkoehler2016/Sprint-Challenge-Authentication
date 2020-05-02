75a2f9b43749e56c0e89d65eb5ae5757f15947c9
Ben Koehler <bens@Bens-MacBook-Pro.local>
2020-05-01 19:00:30 -0700

    test passed and project done

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -4,11 +4,7 @@ const db = require('../database/dbConfig');
 describe('users model', function() {
 
 
-    describe('test environment', function() {
-        it('should use the testing environment', function() {
-            expect(process.env.DB_ENV).toBe('testing');
-        })
-    })
+    
 
     describe('add()', function() {
         beforeEach(async () => {
